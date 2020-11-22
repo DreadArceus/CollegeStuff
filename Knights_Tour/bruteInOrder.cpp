f235ce9307f9d3178b701ecbd0c364593677d3e5
Mayank Talwar <dcmtalwar@gmail.com>
2020-11-22 12:35:49 +0530

    Updated

--- Knights_Tour/bruteInOrder.cpp
+++ Knights_Tour/bruteInOrder.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+bool bad = false;
 bool validityCheck(vector<vector<int>> m)
 {
     for(auto x : m)
@@ -17,92 +18,46 @@ bool validityCheck(vector<vector<int>> m)
     return true;
 }
 
-vector<vector<int>> recurse(vector<vector<int>> m, int x, int y, int num)
+vector<vector<int>> recurse(vector<vector<int>> m, int x, int y, int num, vector<pair<int, int>> moves)
 {
-    if((x > 0 && y > 1) && m[x - 1][y - 2] == -1)
+    for(int i = 0; i < moves.size(); i++)
     {
-        m[x - 1][y - 2] = num;
-        auto loc = recurse(m, x - 1, y - 2, num + 1);
-        if(loc[0][0] != -1)
+        if((x < m.size() + moves[i].first && y < m.size() + moves[i].second) && (x > moves[i].first - 1 && y > moves[i].second - 1)
+            && m[x - moves[i].first][y - moves[i].second] == -1)
         {
-            return loc;
-        }
-    }
-    if((y > 1 && x < m.size() - 1) && m[x + 1][y - 2] == -1)
-    {
-        m[x + 1][y - 2] = num;
-        auto loc = recurse(m, x + 1, y - 2, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x > 0 && y < m.size() - 2) && m[x - 1][y + 2] == -1)
-    {
-        m[x - 1][y + 2] = num;
-        auto loc = recurse(m, x - 1, y + 2, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x < m.size() - 1 && y < m.size() - 2) && m[x + 1][y + 2] == -1)
-    {
-        m[x + 1][y + 2] = num;
-        auto loc = recurse(m, x + 1, y + 2, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x > 1 && y > 2) && m[x - 2][y - 1] == -1)
-    {
-        m[x - 2][y - 1] = num;
-        auto loc = recurse(m, x - 2, y - 1, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x < m.size() - 2 && y > 0) && m[x + 2][y - 1] == -1)
-    {
-        m[x + 2][y - 1] = num;
-        auto loc = recurse(m, x + 2, y - 1, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x > 1 && y < m.size() - 1) && m[x - 2][y + 1] == -1)
-    {
-        m[x - 2][y + 1] = num;
-        auto loc = recurse(m, x - 2, y + 1, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
-        }
-    }
-    if((x < m.size() - 2 && y < m.size() - 1) && m[x + 2][y + 1] == -1)
-    {
-        m[x + 2][y + 1] = num;
-        auto loc = recurse(m, x + 2, y + 1, num + 1);
-        if(loc[0][0] != -1)
-        {
-            return loc;
+            m[x - moves[i].first][y - moves[i].second] = num;
+            m = recurse(m, x - moves[i].first, y - moves[i].second, num + 1, moves);
+            if(!bad)
+            {
+                return m;
+            }
+            m[x - moves[i].first][y - moves[i].second] = -1;
+            bad = false;
         }
     }
     if(!validityCheck(m))
     {
-        m[0][0] = -1;
+        bad = true;
+    /*  Testing
+        system("clear");
+        for(vector<int> v : m)
+        {
+            for(int i : v)
+            {
+                cout << i << " ";
+            }
+            cout << "\n";
+        } */
     }
     return m;
 }
 
 vector<vector<int>> getKnightsMoves(int n, int x, int y)
 {
+    vector<pair<int, int>> vPossible = {{1, 2}, {2, 1}, {-1, 2}, {2, -1}, {-1, -2}, {-2, -1}, {1, -2}, {-2, 1}};
     vector<vector<int>> m(n, vector<int>(n, -1));
     m[x][y] = 0;
-    m = recurse(m, x, y, 1);
+    m = recurse(m, x, y, 1, vPossible);
     return m;
 }
 
